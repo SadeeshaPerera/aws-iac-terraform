74fa02d042dec3d8a375375b0f6d8f04493920e3
Sadeesha Perera <121984253+SadeeshaPerera@users.noreply.github.com>
2025-06-24 23:39:55 +0000

    Remove unused output variables and retain EC2 instance IDs in outputs

--- highly-available-webapp/outputs.tf
+++ highly-available-webapp/outputs.tf
@@ -1,24 +1,6 @@
-output "alb_dns_name" {
-  description = "DNS name of the ALB"
-  value       = aws_lb.webapp.dns_name
+output "ec2_instance_ids" {
+  description = "Instance IDs of EC2 instances in the ASG"
+  value       = aws_autoscaling_group.webapp.*.id
 }
 
-output "rds_endpoint" {
-  description = "RDS endpoint"
-  value       = aws_db_instance.webapp.endpoint
-}
-
-data "aws_autoscaling_group" "webapp" {
-  name = aws_autoscaling_group.webapp.name
-}
-
-# Fetch public IPs for each instance in the ASG
-data "aws_instance" "webapp" {
-  for_each = toset(data.aws_autoscaling_group.webapp.instances)
-  instance_id = each.value
-}
-
-output "ec2_public_ips" {
-  description = "Public IPs of EC2 instances (if any)"
-  value = [for i in data.aws_instance.webapp : i.value.public_ip if i.value.public_ip != null]
-}
+# To get public IPs, use AWS CLI or console, or consider using a custom script or external data source.
